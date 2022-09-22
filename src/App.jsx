b64671ca8e2d91f0c564b02734298c47c21c0f02
symmetryj <mohammadsadry8090@gmail.com>
2022-09-22 19:24:20 +0330

    card component basic des

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Card from "../components/Card";
+
+function App() {
+  return (
+    <div>
+      <Card title="better call sual" rating="4.8" price="$24.99" />
+      <Card title="better call sual" rating="4.8" price="$24.99" />
+      <Card title="better call sual" rating="4.8" price="$24.99" />
+      <Card title="better call sual" rating="4.8" price="$24.99" />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/main.jsx",
+    "./src/App.jsx",
+    "./components/Card.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
